bee5685f674118f770f3947e2fec20b59292f6b5
Michele-Reymond <hello@michelereymond.ch>
2023-05-30 16:09:55 +0200

    added stylesheets components for sharing css trought the app

--- srcs/frontend/src/views/HomeView.vue
+++ srcs/frontend/src/views/HomeView.vue
@@ -1,11 +1,11 @@
 <template>
       <TransButton>pouette</TransButton>
-      <router-link to="/directories"><span>directories</span></router-link>
-      <router-link to="/profile"><span>profile</span></router-link>
-      <router-link to="/game-settings"><span>game settings</span></router-link>
-      <router-link to="/game"><span>game</span></router-link>
-      <router-link to="/chat"><span>chat</span></router-link>
-      <router-link to="/about"><span>about</span></router-link>
+      <router-link class="t-btn-pink" to="/directories"><span>directories</span></router-link>
+      <router-link class="t-btn-pink" to="/profile"><span>profile</span></router-link>
+      <router-link class="t-btn-pink" to="/game-settings"><span>game settings</span></router-link>
+      <router-link class="t-btn-pink" to="/game"><span>game</span></router-link>
+      <router-link class="t-btn-pink" to="/chat"><span>chat</span></router-link>
+      <router-link class="t-btn-pink" to="/about"><span>about</span></router-link>
 </template>
 
 <script setup>
