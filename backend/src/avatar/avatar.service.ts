95e66d0e371bd2cc3dc14690571f910a756a6df4
Davi Farhi <davifarhi@gmail.com>
2023-06-12 10:20:24 +0200

    delete old avatar image when uploading new

--- backend/src/avatar/avatar.service.ts
+++ backend/src/avatar/avatar.service.ts
@@ -1,8 +1,8 @@
 import { HttpService } from '@nestjs/axios';
-import { Injectable, Logger } from '@nestjs/common';
+import { BadRequestException, Injectable, Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { createWriteStream } from 'fs';
-import { join } from 'path';
+import { createWriteStream, unlink } from 'fs';
+import { join, parse } from 'path';
 import { PrismaService } from 'src/prisma.service';
 import { v4 } from 'uuid';
 
@@ -22,16 +22,34 @@ export class AvatarService {
     );
   }
 
-  //TODO delete previous avatar (if not default.jpg)
   async uploadAvatar(user_id: number, filename: string) {
-    const avatar_url = await this.prisma.user.update({
-      where: { id: user_id },
-      data: {
-        avatar_url: this.avatarUrl(filename),
-      },
-      select: { avatar_url: true },
-    });
-    return avatar_url;
+    try {
+      const old_avatar = parse(
+        (
+          await this.prisma.user.findUnique({
+            where: { id: user_id },
+            select: { avatar_url: true },
+          })
+        ).avatar_url,
+      ).base;
+      const avatar_url = await this.prisma.user.update({
+        where: { id: user_id },
+        data: {
+          avatar_url: this.avatarUrl(filename),
+        },
+        select: { avatar_url: true },
+      });
+
+      if (old_avatar !== 'default.jpg')
+        unlink(join('uploads', old_avatar), (err) => {
+          if (err) Logger.error(err);
+        });
+
+      return avatar_url;
+    } catch (e) {
+      Logger.error(`AvatarService::uploadAvatar: ${e}`);
+      throw new BadRequestException();
+    }
   }
 
   async generateAvatar() {
