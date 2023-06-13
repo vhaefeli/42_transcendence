2b97840c1f5333387877bb028590147d15271820
Davi Farhi <davifarhi@gmail.com>
2023-06-13 12:02:20 +0200

    vue added proxy for backend API

--- frontend/src/stores/PlayerStore.ts
+++ frontend/src/stores/PlayerStore.ts
@@ -1,46 +1,45 @@
-import axios from 'axios'
-import { defineStore } from 'pinia'
+import axios from "axios";
+import { defineStore } from "pinia";
 
 type Player = {
-    username: string
-    friends: object
-  }
+  username: string;
+  friends: object;
+};
 
 type State = {
-    player: Player[]
-    isRequestLoading: boolean
-}
+  player: Player[];
+  isRequestLoading: boolean;
+};
 
-export const usePlayerStore = defineStore('playerStore', {
-    state: (): State => ({
-        player: [],
-        isRequestLoading: false,
-      }),
-     actions: {
-        async getUsers(): Promise<void> {
-          let info = null
-          try {
-            const res = await axios.get('http://localhost:3000/user/all').then((response) => (info = response))
-            const status = res.status
-            console.log('req status: ' + status)
-            this.player[0].friends = res.data
-          } catch (e) {
-            console.log(e)
-          }
-        },
-        async getUserName(): Promise<void> {
-            const promise: Promise<Player[]> = new Promise((resolve) => {
-                resolve([
-                    {
-                        username: 'pouette',
-                        friends: [
-                            {id: 1, username: "kevin"},
-                            {id: 2, username: "Sabine"}
-                        ],
-                    },
-                ])
-            })
-            this.player = await promise
-        },
-    }
-})
+export const usePlayerStore = defineStore("playerStore", {
+  state: (): State => ({
+    player: [],
+    isRequestLoading: false,
+  }),
+  actions: {
+    async getUsers(): Promise<void> {
+      try {
+        const response = await axios
+          .get("/api/user/all")
+        console.log("req status: " + response.status);
+        this.player[0].friends = response.data;
+      } catch (e) {
+        console.log(e);
+      }
+    },
+    async getUserName(): Promise<void> {
+      const promise: Promise<Player[]> = new Promise((resolve) => {
+        resolve([
+          {
+            username: "pouette",
+            friends: [
+              { id: 1, username: "kevin" },
+              { id: 2, username: "Sabine" },
+            ],
+          },
+        ]);
+      });
+      this.player = await promise;
+    },
+  },
+});
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -10,5 +10,14 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://backend:3000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 })
