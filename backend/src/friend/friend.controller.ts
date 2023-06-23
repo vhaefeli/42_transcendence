710f09a2d691ff37747a6a371232eccb6dce29c6
Davi Farhi <davifarhi@gmail.com>
2023-06-23 15:52:16 +0200

    changed request names

--- backend/src/friend/friend.controller.ts
+++ backend/src/friend/friend.controller.ts
@@ -12,19 +12,19 @@ import { FriendService } from './friend.service';
 export class FriendController {
   constructor(private friendService: FriendService) {}
 
-  @Post(':username')
+  @Post('invite/:username')
   async sendFriendshipInvitation(@Param() params: any, @Request() req: any) {
     if (req.user.username == params.username) throw new ConflictException();
     await this.friendService.createInvitation(req.user.sub, params.username);
     return;
   }
 
-  @Get('view')
+  @Get('invite/received')
   async getInvitations(@Request() req: any) {
     return await this.friendService.findInvitationsReceived(req.user.sub);
   }
 
-  @Post('accept/:from_username')
+  @Post('invite/accept/:from_username')
   async acceptInvitation(@Param() params: any, @Request() req: any) {
     await this.friendService.acceptInvitation(
       params.from_username,
