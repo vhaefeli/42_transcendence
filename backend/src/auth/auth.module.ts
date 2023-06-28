f0266df56454263bf83c1dde34f82a871626cf6d
Davi Farhi <davifarhi@gmail.com>
2023-06-28 19:42:18 +0200

    Revert "Twilio 2fa module and added necessary varenvs"
    This reverts commit d6b2333fde60023a3ec0eaed6648ea85891b706c.

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -7,23 +7,14 @@ import { AuthGuard } from './auth.guard';
 import { AuthService } from './auth.service';
 import { JwtConfigService } from './jwt.service';
 import { WsGuard } from './ws.guard';
-import { TwilioModule } from 'nestjs-twilio';
-import { TwilioConfigService } from './twilio.service';
 
 @Module({
   controllers: [AuthController],
-  providers: [
-    AuthService,
-    JwtConfigService,
-    AuthGuard,
-    WsGuard,
-    TwilioConfigService,
-  ],
+  providers: [AuthService, JwtConfigService, AuthGuard, WsGuard],
   imports: [
     forwardRef(() => UserModule),
     HttpModule,
     JwtModule.registerAsync({ useClass: JwtConfigService, global: true }),
-    TwilioModule.forRootAsync({ useClass: TwilioConfigService }),
   ],
   exports: [AuthGuard, AuthService, WsGuard],
 })
