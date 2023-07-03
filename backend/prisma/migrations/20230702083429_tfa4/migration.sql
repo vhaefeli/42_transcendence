b0c633ee16fb41200e019b0a5222942d85372f68
Davi Farhi <davifarhi@gmail.com>
2023-07-03 10:47:29 +0200

    group all migrations for 2fa into one

--- backend/prisma/migrations/20230702083429_tfa4/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateEnum
-CREATE TYPE "TfaRegistrationType" AS ENUM ('ENABLE', 'DISABLE');
-
--- AlterTable
-ALTER TABLE "TfaRegistration" ADD COLUMN     "type" "TfaRegistrationType" NOT NULL DEFAULT 'ENABLE';
