01add5041f6058afbe0fac6eb0c2b8b1042d28bd
Valerie Roch <valerie.roch@nsecfl.ch>
2023-07-12 13:53:20 +0200

    retrait prisma fromplayer.module.ts

--- backend/src/player/player.module.ts
+++ backend/src/player/player.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { PlayerController } from './player.controller';
 import { PlayerService } from './player.service';
-import { PrismaService } from 'src/prisma.service';
 
 @Module({
   controllers: [PlayerController],
-  providers: [PlayerService, PrismaService],
+  providers: [PlayerService],
 })
 export class PlayerModule {}
