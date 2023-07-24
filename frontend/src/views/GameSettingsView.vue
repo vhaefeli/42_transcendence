8a2c4cb7c47143e92d89e95662543566588271f2
Vhaefeli <vhaefeli@student.42lausanne.ch>
2023-07-24 11:36:49 +0200

    pause pour bouton

--- frontend/src/views/GameSettingsView.vue
+++ frontend/src/views/GameSettingsView.vue
@@ -1,16 +1,41 @@
 <template>
-  <NavBar />
-  <div>Setup a game here</div>
-  <div class="text-white">
-    <input
-      v-model="gameIdToConnect"
-      placeholder="game id"
-      class="bg-gray-500"
-    /><br />
+   <NavBar :showProfile="true"></NavBar>
+  <div id="gamesetting-container">
+    <div class="text-white">
+      <input
+        v-model="gameIdToConnect"
+        placeholder="game id"
+        class="bg-gray-500"
+      /><br />
+    </div>
+    <button class="t-btn-pink ft-color-game" id="play" @click="connectToGame">
+      <span>Play Game</span>
+    </button>
+    <div class="flex flex-col text-center ft-left-tab ft-my-profile" id="friends-search" :class="{ foreground: foregroundTab === 'search' }" @click="setForegroundTab('search')">
+        <div class="ft-tab-folder ft-tab-title ft-bb-color-profile">Add a new friend</div>
+        <div class="ft-tab-content ft-border-color-profile ft-tab-border text-left">
+            <div class="flex flex-row justify-center">
+              <ModelListSelect
+                :list="userList"
+                v-model="selectedUser"
+                optionValue="id"
+                optionText="username"
+                placeholder="Search by username"
+              />
+              <div :class="{ 'cursor-not-allowed': !selectedUser }">
+                <a
+                  @click="validateSelection"
+                  class="t-btn-pink ft-color-add ft-icon-small icon-btn-size icon-btn-cursor"
+                  :class="{ 'opacity-50 searchan-noClick': !selectedUser }">
+                  <img src="../assets/icons/user-plus-solid.svg" alt="send a friend request" title="send them a friend request">
+                </a>
+              </div>
+              <!-- <input type="text" placeholder="Search by username"> -->
+              <!-- <a class="t-btn-pink ft-color-add ft-icon-small icon-btn-size icon-btn-cursor"><img src="../assets/icons/user-plus-solid.svg" alt="send a friend request" title="send them a friend request"></a> -->
+            </div>
+        </div>
+      </div>
   </div>
-  <button class="t-btn-pink ft-color-game" id="play" @click="connectToGame">
-    <span>Play Game</span>
-  </button>
 </template>
 
 <script setup>
@@ -28,12 +53,23 @@ function connectToGame() {
 }
 </script>
 
-<style>
+<style scoped>
+
+
+#gamesetting-container {
+  background: var(--gray);
+    border: 4px solid var(--sunset);
+  padding: 5vw;
+    
+    border-radius: 25px 25px 0 0;
+    overflow: hidden;
+}
+
 #play {
-  position: absolute;
-  top: 20vw;
-  left: 50%;
-  transform: translateX(-50%);
+  /* position: absolute; */
+  /* top: 20vw; */
+  /* left: 50%;
+  transform: translateX(-50%); */
   background-color: var(--pink);
 }
 </style>
