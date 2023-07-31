89da579b83de7e176648a66a32a1b5c5d6047909
Nadia Chennaf <84449287+ndcnf@users.noreply.github.com>
2023-07-31 17:03:22 +0200

    avatar change position

--- frontend/src/views/EditProfileView.vue
+++ frontend/src/views/EditProfileView.vue
@@ -97,11 +97,58 @@
 
 
     <!-- dossier gauche central pour avatar change -->
+    <div class="flex flex-col text-center ft-left-tab" id="avatar-change" :class="{ foreground: foregroundTab === 'avatar-change' }" @click="setForegroundTab('avatar-change')">
+      <div class="ft-tab-folder ft-tab-title ft-bb-color-profile">Two factor authentication</div>
+      <div class="ft-tab-content ft-border-color-profile ft-tab-border flex flex-col justify-evenly text-left">
+        <p class="text-white">Upload a new avatar for your profile</p>
+        <div
+          class="drop-area w-2/3"
+          :data-active="active"
+          @dragenter.prevent="setActive"
+          @dragover.prevent="setActive"
+          @dragleave.prevent="setInactive"
+          @drop.prevent="onDrop"
+        >
+          <p>Drop your image here</p>
+          <label for="file-input">or select a file:</label>
+          <input
+            type="file"
+            id="selectedFile"
+            style="display: none"
+            name="file-input"
+            v-on:change="fileInputOnChange"
+          />
+          <input
+            class="ft-browse-button"
+            type="button"
+            value="Browse..."
+            onclick="document.getElementById('selectedFile').click();"
+          />
+          <p v-if="selectedAvatar == undefined">no image selected</p>
+          <p v-if="selectedAvatar" class="truncate">{{ selectedAvatar.name }}</p>
+          <slot :dropZoneActive="active"></slot>
+          <div
+            :class="{ 'cursor-not-allowed': selectedAvatar == undefined }"
+            class="w-fit"
+          >
+            <button
+              class="ft-edit-button"
+              @click="uploadNewAvatar"
+              :class="{ 'opacity-50 ft-noClick': selectedAvatar == undefined }"
+            >
+              Upload
+            </button>
+          </div>
+        </div>
+
+
+      </div>
+    </div>
 
 
 
       <!-- AVATAR -->
-      <p class="text-white">Upload a new avatar for your profile</p>
+      <!-- <p class="text-white">Upload a new avatar for your profile</p>
       <div
         class="drop-area w-2/3"
         :data-active="active"
@@ -140,35 +187,8 @@
             Upload
           </button>
         </div>
-      </div>
-
-      <!-- USERNAME -->
-      <!-- <div class="text-white" id="ft-edit-username">
-        <div v-if="!usernameChanged">
-          <p>Modify your username:</p>
-          <input
-            class="min-w-10 ft-text-input"
-            v-model="username"
-            placeholder="new username"
-          />
-          <div
-            :class="{ 'cursor-not-allowed': username.length == 0 }"
-            class="w-fit"
-          >
-            <button
-              class="ft-edit-button"
-              @click="submitNewUsername"
-              :class="{ 'opacity-50 ft-noClick': username.length == 0 }"
-            >
-              Submit
-            </button>
-          </div>
-        </div>
-        <div v-if="usernameChanged">
-          <p>Username successfully changed, please login again</p>
-          <button class="t-btn-pink" @click="logout">Go to Login</button>
-        </div>
       </div> -->
+
     </section>
   </div>
 
@@ -571,18 +591,28 @@ function submitNewUsername() {
 
 .ft-left-tab#two-fa {
   position: relative;
-  top: -20rem;
+  top: -30rem;
   left: 8vw;
   width: 25rem;
 
   z-index: 1;
 }
 
+
 .ft-right-tab#username-change {
   position: relative;
   top: -35rem;
   left: 65vw;
   width: 25rem;
+  
+  z-index: 1;
+}
+
+.ft-left-tab#avatar-change {
+  position: relative;
+  top: -35rem;
+  left: 12vw;
+  width: 70rem;
 
   z-index: 1;
 }
