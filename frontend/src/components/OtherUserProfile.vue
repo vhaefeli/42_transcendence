4cc8aa079a7720f23e9d6c82405eef4e5fb452d4
Michele-Reymond <hello@michelereymond.ch>
2023-08-07 20:41:52 +0200

    fix no display of admin panel in Dms in other user profile

--- frontend/src/components/OtherUserProfile.vue
+++ frontend/src/components/OtherUserProfile.vue
@@ -35,7 +35,7 @@
                   </div>
               </div>
           </div>
-          <div v-if="props.currentChannel?.Admin != 'null'" class="flex flex-col items-center text-center ft-central-tab-container mb-3">
+          <div v-if="props.currentChannel && props.currentChannel?.Admin != 'null'" class="flex flex-col items-center text-center ft-central-tab-container mb-3">
             <div class="ft-tab-content ft-bg-color-profile flex flex-col items-center">
               <h3 class="py-1">manage acess to channel</h3>
               <div class="flex">
